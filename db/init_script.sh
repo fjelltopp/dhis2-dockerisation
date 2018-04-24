c33448dd9160b62e4b55e7d4f90aebf5c43418be
jyri <Jyri Soppela>
2018-04-24 13:58:41 +0300

    init script fixes (incomplete)

--- db/init_script.sh
+++ db/init_script.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-psql -U postgres -c "DROP USER ${POSTGRES_USER};"
-psql -U postgres -c "CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';"
-psql -U postgres -c "ALTER USER ${POSTGRES_USER} CREATEDB;"
-psql -U postgres -c "ALTER USER ${POSTGRES_USER} CREATEROLE;"
-psql -U postgres -c "DROP DATABASE ${POSTGRES_DB};"
-psql -U postgres -c "CREATE DATABASE ${POSTGRES_DB};"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE pg_database TO ${POSTGRES_USER};"
-psql -U ${POSTGRES_USER} ${POSTGRES_DB} < /tmp/dhis_init.sql
+psql -U postgres -c "CREATE USER ${DHIS2_PG_USER} WITH PASSWORD '${DHIS2_PG_PASSWORD}';"
+psql -U postgres -c "ALTER USER ${DHIS2_PG_USER} CREATEDB;"
+psql -U postgres -c "ALTER USER ${DHIS2_PG_USER} CREATEROLE;"
+psql -U postgres -c "CREATE DATABASE ${DHIS2_PG_DB};"
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${DHIS2_PG_DB} TO ${DHIS2_PG_USER};"
+psql -U ${DHIS2_PG_USER} ${DHIS2_PG_DB} < /tmp/dhis_init.sql
 
 
 
