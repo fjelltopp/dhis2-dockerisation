f2df0640a299f54ca21af567d7d74471c4e8deb8
jyri <Jyri Soppela>
2018-04-24 10:48:32 +0300

    refactor init scripts

--- db/init_script.sh
+++ db/init_script.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-psql -U postgres -c 'CREATE USER ${POSTGRES_USER} WITH PASSWORD ''${POSTGRES_PASSWORD}'';'
-psql -U postgres -c 'DROP DATABASE ${POSTGRES_DB};'
-psql -U postgres -c 'CREATE DATABASE ${POSTGRES_DB};'
-psql -U postgres -c 'GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};'
+psql -U postgres -c "DROP USER ${POSTGRES_USER};"
+psql -U postgres -c "CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';"
+psql -U postgres -c "ALTER USER ${POSTGRES_USER} CREATEDB;"
+psql -U postgres -c "ALTER USER ${POSTGRES_USER} CREATEROLE;"
+psql -U postgres -c "DROP DATABASE ${POSTGRES_DB};"
+psql -U postgres -c "CREATE DATABASE ${POSTGRES_DB};"
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};"
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE pg_database TO ${POSTGRES_USER};"
 psql -U ${POSTGRES_USER} ${POSTGRES_DB} < /tmp/dhis_init.sql
 
 
