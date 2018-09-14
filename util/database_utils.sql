c2dd0b0505f81cadc52a33e6cd34adeee8707f3d
jyri <Jyri Soppela>
2018-09-14 16:53:05 +0300

    add SQL scripts for removing org unit speficic data value audits

--- util/database_utils.sql
+++ util/database_utils.sql
@@ -10,3 +10,16 @@ delete from datavalueaudit dva where dva.dataelementid in
  select dataelementid from dataelement where name like 'HOQM%'
 );
 
+-- get data valua audits for organisation id
+select *
+from datavalueaudit da
+join organisationunit ou
+ on da.organisationunitid=ou.organisationunitid
+where ou.uid='iPjpfPR1Ptd';
+
+-- delete all data value audits for organisation id
+delete
+from datavalueaudit da
+using organisationunit ou
+ where da.organisationunitid=ou.organisationunitid
+ and ou.uid='iPjpfPR1Ptd';
