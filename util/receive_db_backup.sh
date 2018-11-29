c8fbec513887b59b5ddf3180c07ac3584960fda3
Tomasz Sabała <sabala.tomek@gmail.com>
2018-11-29 20:46:58 +0100

    Fixed receive backup sh script.

--- util/receive_db_backup.sh
+++ util/receive_db_backup.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash -e
-mv ~/db_backup.sql ~/dhis2-dockerisation/${COUNTRY}/
+#!/usr/bin/env bash
+set -e
+mv ~/db-backup.sql ~/dhis2-dockerisation/${COUNTRY}/
 cd ~/dhis2-dockerisation
+set +e
+set +x
 docker-compose -f docker-compose-${COUNTRY}.yml -f docker-compose-ssl.yml down
 docker volume ls -qf dangling=true | xargs -r docker volume rm
 docker-compose -f docker-compose-${COUNTRY}.yml -f docker-compose-ssl.yml up -d
