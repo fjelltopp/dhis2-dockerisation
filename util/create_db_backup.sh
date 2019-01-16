00bdd332a23ff5effc6bd96a4e5b676f6778aa39
Ubuntu <ubuntu@fgs-hmis.hoqm.org>
2019-01-16 08:08:32 +0000

    fixes for training sync scripts

--- util/create_db_backup.sh
+++ util/create_db_backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-docker exec -it dhis2-database bash -c 'pg_dump --username=$POSTGRES_USER --dbname=$POSTGRES_DB > db-backup.sql'
+docker exec dhis2-database bash -c 'pg_dump --username=$POSTGRES_USER --dbname=$POSTGRES_DB > db-backup.sql'
 docker cp dhis2-database:/db-backup.sql .
-docker exec -it dhis2-database bash -c 'rm -f db-backup.sql'
+docker exec dhis2-database bash -c 'rm -f db-backup.sql'
